f8b0c1048f3e30174962d970141a6c5048a790c6
Zdy <zhudongya123@126.com>
2019-04-23 20:30:33 +0800

    try add send request

--- Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/plugins/addition/hook/AutoSendSnsRequestHook.kt
+++ Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/plugins/addition/hook/AutoSendSnsRequestHook.kt
@@ -18,7 +18,10 @@ import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import org.xml.sax.InputSource
 import java.io.StringReader
-import java.util.ArrayList
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
+import java.util.*
 
 object AutoSendSnsRequestHook {
     fun hook(classLoader: ClassLoader) {
@@ -27,6 +30,7 @@ object AutoSendSnsRequestHook {
         val dataClass = XposedHelpers.findClass("com.tencent.mm.pluginsdk.model.m", classLoader)
         val requestDataClass = XposedHelpers.findClass("com.tencent.mm.ai.m", classLoader)
         val requestHandlerClass = XposedHelpers.findClass("com.tencent.mm.model.aw", classLoader)
+        val wechatRequestHandlerClass = XposedHelpers.findClass("com.tencent.mm.plugin.messenger.a.f", classLoader)
 
 
         val ContactInfoUI = XposedHelpers.findClass("com.tencent.mm.plugin.profile.ui.ContactInfoUI", classLoader)
@@ -61,6 +65,44 @@ object AutoSendSnsRequestHook {
                 })
 
 
+                XposedHelpers.callMethod(thisObject, "addTextOptionMenu", 1, "加", object : MenuItem.OnMenuItemClickListener {
+                    override fun onMenuItemClick(item: MenuItem?): Boolean {
+
+                        val interface1 = XposedHelpers.findClass("com.tencent.mm.ai.f", classLoader)
+                        val array = Array<Class<*>>(1, init = { interface1 })
+                        val newObject = Proxy.newProxyInstance(classLoader, array, object : InvocationHandler {
+                            override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any? {
+                                args?.let {
+                                    XposedBridge.log("AutoSendSnsRequestHook, newProxyInstance , method = $method, args = ${args.joinToString { if (it == null) "null" else it.toString() }}")
+                                }
+                                if (method == null) {
+                                    return null
+                                } else return when {
+                                    String::class.java == method.returnType -> ""
+                                    Int::class.java == method.returnType -> Integer.valueOf(0)
+                                    Int::class.javaPrimitiveType == method.returnType -> 0
+                                    Boolean::class.java == method.returnType -> java.lang.Boolean.FALSE
+                                    Boolean::class.javaPrimitiveType == method.returnType -> false
+                                    else -> null
+                                }
+                            }
+                        })
+
+                        val nameText = "zhuyier"
+                        val Rg = XposedHelpers.callStaticMethod(XposedHelpers.findClass("com.tencent.mm.kernel.g", classLoader), "Rg")
+                        XposedHelpers.callMethod(Rg, "a", 106, newObject)
+
+                        val f = XposedHelpers.findClass("com.tencent.mm.plugin.messenger.a.f", classLoader)
+                        val constructor = f.getConstructor(String::class.java, Int::class.java)
+                        val fVar = constructor.newInstance(nameText, 3)
+
+                        XposedHelpers.callMethod(Rg, "a", fVar, 0)
+
+                        return true
+                    }
+                })
+
+
             }
         })
 
@@ -77,13 +119,79 @@ object AutoSendSnsRequestHook {
                         val args = param.args
 
 
-                        XposedBridge.log("AutoSendSnsRequestHook, args = ${args.joinToString { if (it == null) "null" else it.toString() }}")
+                        XposedBridge.log("AutoSendSnsRequestHook, com.tencent.mm.pluginsdk.model.m, Constructor args = ${args.joinToString { if (it == null) "null" else it.toString() }}")
 
 
                     }
                 })
 
 
+//        a(int i, int i2, int i3, java.lang.String str, com.tencent.mm.network.q qVar, byte[] bArr)
+
+        val argClass = XposedHelpers.findClass("com.tencent.mm.network.q", classLoader)
+        val aaClass = XposedHelpers.findClass("com.tencent.mm.platformtools.aa", classLoader)
+
+        XposedHelpers.findAndHookMethod(wechatRequestHandlerClass, "a",
+                Int::class.java, Int::class.java, Int::class.java,
+                String::class.java, argClass, ByteArray::class.java,
+
+                object : XC_MethodHook() {
+
+                    override fun afterHookedMethod(param: MethodHookParam) {
+
+                        val args = param.args
+
+                        XposedBridge.log("AutoSendSnsRequestHook, args = ${args.joinToString { if (it == null) "null" else it.toString() }}")
+
+
+                        val thisObject = param.thisObject
+
+                        val bOj = XposedHelpers.callMethod(thisObject, "bOj")//com.tencent.mm.protocal.protobuf.buv
+
+                        val wcB = XposedHelpers.getObjectField(bOj, "wcB")
+
+                        /////com.tencent.mm.platformtools.aa.a(buv.wcB)
+
+                        val result = XposedHelpers.callStaticMethod(aaClass, "a", wcB) as String
+
+                        XposedBridge.log("AutoSendSnsRequestHook, result = ${result}")
+
+
+                        //添加好友
+//                        val constructor = XposedHelpers.findConstructorExact(dataClass,
+//                                Int::class.java, java.util.List::class.java, java.util.List::class.java, java.util.List::class.java,
+//                                String::class.java, String::class.java, java.util.Map::class.java,
+//                                String::class.java, String::class.java)
+//                        constructor.isAccessible = true
+//
+//                        val m = constructor.newInstance(2, ArrayList<String>().apply { add(result) }, ArrayList<Int>().apply { add(3) }, null,
+//                                "测试发送好友请求", "", HashMap<String, Int>().apply { put(result, 0) },
+//                                null, ""
+//                        )
+//
+//                        val auDF = XposedHelpers.callStaticMethod(requestHandlerClass, "Rg")
+//                        XposedHelpers.callMethod(auDF, "a", m, 0)
+
+                    }
+                }
+        )
+
+
+        val fClass = XposedHelpers.findClass("com.tencent.mm.plugin.messenger.a.f", classLoader)
+
+        XposedHelpers.findAndHookConstructor(fClass, String::class.java, Int::class.java, Int::class.java, Boolean::class.java,
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+
+                        val args = param.args
+
+                        XposedBridge.log("AutoSendSnsRequestHook, com.tencent.mm.plugin.messenger.a.f, Constructor args = ${args.joinToString { if (it == null) "null" else it.toString() }}")
+
+                    }
+                })
+
+
     }
+
 
 }
